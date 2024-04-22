e4ae2a7135429f90d1f87fe2ad4c322f2d126e9d
BlackpearlJack <lestercarlyle@gmail.com>
2024-04-23 01:59:28 +0300

    crud backend, admin configuration, css edits on frontend

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ mongoose.connect("mongodb+srv://lestercarlyle:N1kl%40us99@cluster0.c2zbmo6.mongo
 app.get("/",(req,res)=>{
     res.send("Express App is running");
 })
+
+
 // image storage engine
 
 const stg = multer.diskStorage({
@@ -33,6 +35,7 @@ const upload = multer({storage: stg});
 app.use('/images/', express.static('upload/images')) // corrected line
 
 // Creating Upload Endpoint for image
+
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success: 1,
@@ -40,7 +43,7 @@ app.post("/upload",upload.single('product'),(req,res)=>{
     })
 })
 
-// Schems for creating products
+// Schemas for creating products
 
 const Product = mongoose.model("Product",{
     id:{
@@ -77,21 +80,63 @@ const Product = mongoose.model("Product",{
     },
     available:{
         type: Boolean,
-        required: true,
+        default: true,
     },
 })
 
+// Creating API for adding products
+
 app.post("/addproduct",async(req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length > 0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id + 1;
+    }else{
+        id = 1;
+    }
     const product = new Product({
-        id: req.body.id,
+        id: id,
         name: req.body.name,
         image: req.body.image,
         category: req.body.category,
         description: req.body.description,
         new_price: req.body.new_price,
         old_price: req.body.old_price,
-        available: req.body.available,
-    })
+    });
+
+    try {
+        await product.save();
+        console.log("Product added successfully");
+        res.json({
+          success: true,
+          name: req.body.name,
+        });
+
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Server error'+error });
+      }
+})
+
+// Creatin API for deleting all products
+
+app.post("/removeproduct", async (req,res) => {
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Product deleted successfully");
+    res.json({
+        success: true,
+        name:req.body.name,
+    });
+})
+
+// Creating API for getting all products
+
+app.get("/allproducts", async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All products fetched successfully");
+    res.send(products);
 })
 
 app.listen(port,(error)=>{
